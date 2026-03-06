7ce2752bc62368a489d4e8e07928625942e0b0ec
mordelonsangucheria <sangucheriamordelon@gmail.com>
2026-03-06 17:44:40 -0300

    Delete vendedor-sw-register.js

--- vendedor-sw-register.js
+++
@@ -1,5 +0,0 @@
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw-vendedor.js')
-      .then(() => console.log('SW registrado'))
-      .catch(e => console.log('SW error:', e));
-  }
